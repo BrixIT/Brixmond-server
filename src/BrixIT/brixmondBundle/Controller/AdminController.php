f3de48386e97f52d44b03d5e56195ba059a901b8
Martijn Braam <martijn@brixit.nl>
2015-04-22 14:08:34 +0200

    Added 2 basic admin pages

--- src/BrixIT/brixmondBundle/Controller/AdminController.php
+++ src/BrixIT/brixmondBundle/Controller/AdminController.php
@@ -6,8 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class AdminController extends Controller
 {
-    public function indexAction()
+    public function serversAction()
     {
-        return $this->render('');
+        return $this->render('BrixITbrixmondBundle:Admin:servers.html.twig');
+    }
+
+    public function usersAction()
+    {
+        $userManager = $this->get('fos_user.user_manager');
+        $users = $userManager->findUsers();
+        $context = [
+            'users' => $users
+        ];
+        return $this->render('BrixITbrixmondBundle:Admin:users.html.twig', $context);
     }
 }
