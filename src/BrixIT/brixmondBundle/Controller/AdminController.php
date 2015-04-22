61efac41d8251fbc353564fcac0b9cf07062ac43
Martijn Braam <martijn@brixit.nl>
2015-04-22 22:14:17 +0200

    Added support for accepting and rejecting servers

--- src/BrixIT/brixmondBundle/Controller/AdminController.php
+++ src/BrixIT/brixmondBundle/Controller/AdminController.php
@@ -3,12 +3,45 @@
 namespace BrixIT\brixmondBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class AdminController extends Controller
 {
     public function serversAction()
     {
-        return $this->render('BrixITbrixmondBundle:Admin:servers.html.twig');
+        $hostsActive = $this->getDoctrine()->getRepository('BrixITbrixmondBundle:Client')->findBy([
+            'enabled' => true
+        ]);
+        $hostsInactive = $this->getDoctrine()->getRepository('BrixITbrixmondBundle:Client')->findBy([
+            'enabled' => false
+        ]);
+        $context = [
+            'clients' => [
+                'active' => $hostsActive,
+                'inactive' => $hostsInactive
+            ]
+        ];
+        return $this->render('BrixITbrixmondBundle:Admin:servers.html.twig', $context);
+    }
+
+    public function serverAcceptAction($id)
+    {
+        $client = $this->getDoctrine()->getRepository('BrixITbrixmondBundle:Client')->find($id);
+        $client->setEnabled(true);
+        $manager = $this->getDoctrine()->getManager();
+        $manager->persist($client);
+        $manager->flush();
+        return $this->redirectToRoute('admin_servers', [], 303);
+    }
+
+    public function serverRejectAction($id)
+    {
+        $client = $this->getDoctrine()->getRepository('BrixITbrixmondBundle:Client')->find($id);
+        $manager = $this->getDoctrine()->getManager();
+        $manager->remove($client);
+        $manager->flush();
+        return $this->redirectToRoute('admin_servers', [], 303);
     }
 
     public function usersAction()
