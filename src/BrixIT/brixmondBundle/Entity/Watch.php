02a8f3082445750afb0e189b56ef5b7f8c2d29e8
Martijn Braam <martijn@brixit.nl>
2015-04-23 23:53:06 +0200

    Added entity to store watches

---
+++ src/BrixIT/brixmondBundle/Entity/Watch.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace BrixIT\brixmondBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Watch
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Watch
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=10)
+     */
+    private $type;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="system", type="string", length=64)
+     */
+    private $system;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="expression", type="text")
+     */
+    private $expression;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Watch
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     * @return Watch
+     */
+    public function setType($type)
+    {
+        if (!in_array($type, ['point', 'info'])) {
+            throw new \InvalidArgumentException("Invalid type");
+        }
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set system
+     *
+     * @param string $system
+     * @return Watch
+     */
+    public function setSystem($system)
+    {
+        $this->system = $system;
+
+        return $this;
+    }
+
+    /**
+     * Get system
+     *
+     * @return string
+     */
+    public function getSystem()
+    {
+        return $this->system;
+    }
+
+    /**
+     * Set expression
+     *
+     * @param string $expression
+     * @return Watch
+     */
+    public function setExpression($expression)
+    {
+        $this->expression = $expression;
+
+        return $this;
+    }
+
+    /**
+     * Get expression
+     *
+     * @return string
+     */
+    public function getExpression()
+    {
+        return $this->expression;
+    }
+}
