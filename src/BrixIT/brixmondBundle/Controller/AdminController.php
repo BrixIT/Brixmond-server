1481e1bfe34938fba0ca95a323b31b967eca9113
Martijn Braam <martijn@brixit.nl>
2015-04-24 22:12:34 +0200

    Added full CRUD for watches in admin

--- src/BrixIT/brixmondBundle/Controller/AdminController.php
+++ src/BrixIT/brixmondBundle/Controller/AdminController.php
@@ -4,8 +4,10 @@ namespace BrixIT\brixmondBundle\Controller;
 
 use BrixIT\brixmondBundle\Entity\Host;
 use BrixIT\brixmondBundle\Entity\User;
+use BrixIT\brixmondBundle\Entity\Watch;
 use BrixIT\brixmondBundle\Form\HostType;
 use BrixIT\brixmondBundle\Form\UserType;
+use BrixIT\brixmondBundle\Form\WatchType;
 use BrixIT\brixmondBundle\Model\UserNotification;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -175,5 +177,42 @@ class AdminController extends Controller
             'watches' => $watches
         ];
         return $this->render('@BrixITbrixmond/Admin/watches.html.twig', $context);
+    }
+
+    public function watchEditAction(Request $request, $id)
+    {
+        if ($id === 'new') {
+            $watch = new Watch();
+        } else {
+            $watch = $this->getDoctrine()->getRepository('BrixITbrixmondBundle:Watch')->find($id);
+        }
+        $form = $this->createForm(new WatchType(), $watch);
+        $form->add('save', 'submit', array('label' => 'admin.watches.form.save.label'));
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($watch);
+            $manager->flush();
+            return $this->redirectToRoute('admin_watches', [], 303);
+        }
+        $context = [
+            'id' => $id,
+            'form' => $form->createView(),
+            'isnew' => $id === 'new'
+        ];
+        return $this->render('BrixITbrixmondBundle:Admin:watch_edit.html.twig', $context);
+    }
+
+    public function watchRemoveAction($id)
+    {
+        $watch = $this->getDoctrine()->getRepository('BrixITbrixmondBundle:Watch')->find($id);
+
+        $manager = $this->getDoctrine()->getManager();
+        $manager->remove($watch);
+        $manager->flush();
+        return $this->redirectToRoute('admin_watches', [], 303);
+
     }
 }
---
+++ src/BrixIT/brixmondBundle/Form/WatchType.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace BrixIT\brixmondBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class WatchType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', 'text', [
+                'label' => 'admin.watches.form.name.label'
+            ])
+            ->add('type', 'choice', [
+                'choices' => [
+                    'point' => 'admin.watches.types.point',
+                    'info' => 'admin.watches.types.info',
+                ],
+                'label' => 'admin.watches.form.type.label'
+            ])
+            ->add('system', 'text', [
+                'label' => 'admin.watches.form.system.label'
+            ])
+            ->add('expression', 'textarea', [
+                'label' => 'admin.watches.form.expression.label'
+            ])
+            ->add('notificationTitle', 'text', [
+                'label' => 'admin.watches.form.notificationtitle.label'
+            ])
+            ->add('notificationMessage', 'textarea', [
+                'label' => 'admin.watches.form.notificationmessage.label'
+            ])
+            ->add('action', 'choice', [
+                'choices' => [
+                    'info' => 'admin.watches.actions.info',
+                    'warning' => 'admin.watches.actions.warning',
+                    'error' => 'admin.watches.actions.error',
+                    'drop' => 'admin.watches.actions.drop'
+                ],
+                'label' => 'admin.watches.form.action.label'
+            ]);
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'BrixIT\brixmondBundle\Entity\Watch'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'brixit_brixmondbundle_watch';
+    }
+}
